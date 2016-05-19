3180a223359c31eed8cc4f416fd4fa2cc1b7459a
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-19 23:06:58 +0100

    Rename method

--- src/jasentaa/position.clj
+++ src/jasentaa/position.clj
@@ -1,18 +1,17 @@
 (ns jasentaa.position)
 
-(defn emit
+(defn augment-location
   ([text]
-   (emit text 1 1))
+   (augment-location text 1 1))
 
   ([text line col]
    (let [ch (first text)]
      (if-not (nil? ch)
        (cons
-         {:char ch :line line :col col}
+         [ch line col]
          (lazy-seq
-           (emit
+           (augment-location
              (rest text)
              (if (= ch \newline) (inc line) line)
              (if (= ch \newline) 1 (inc col)))))))))
 
-(emit "Hello\nWorld")
