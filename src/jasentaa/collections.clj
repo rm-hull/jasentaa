42b36cdf95fb9a9097221378fc305cd15d28b2fe
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-29 18:30:58 +0100

    Make `join` work against records and strings

--- src/jasentaa/collections.clj
+++ src/jasentaa/collections.clj
@@ -1,17 +1,31 @@
 (ns jasentaa.collections)
 
+(defn- not-list? [x]
+  (or
+    (not (coll? x))
+    (record? x)))
+
 (defn join [a b]
   (cond
+    (and (string? a) (string? b))
+    (str a b)
+
+    (and (nil? a) (string? b))
+    b
+
+    (and (string? a) (nil? b))
+    a
+
     (nil? a)
     (recur [] b)
 
     (nil? b)
     (recur a [])
 
-    (not (list? a))
+    (not-list? a)
     (recur (list a) b)
 
-    (not (list? b))
+    (not-list? b)
     (recur a (list b))
 
     :else
