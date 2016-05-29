cb1450bbb1f37319bd6d5f37c3bfd86ff1768028
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-29 18:30:58 +0100

    Make `join` work against records and strings

--- test/jasentaa/collections_test.clj
+++ test/jasentaa/collections_test.clj
@@ -1,9 +1,10 @@
 (ns jasentaa.collections-test
   (:require
     [clojure.test :refer :all]
-    [jasentaa.collections :refer [join]]))
+    [jasentaa.collections :refer [join]]
+    [jasentaa.position :refer [augment-location]]))
 
-(deftest check-join
+(deftest check-join-on-lists
   (is (= [1 2] (join 1 2)))
   (is (= [3 4] (join [3] 4)))
   (is (= [5 6] (join 5 [6])))
@@ -11,3 +12,14 @@
   (is (= [9]   (join 9 nil)))
   (is (= [0]   (join nil 0)))
   (is (= []    (join nil nil))))
+
+(deftest check-join-on-records
+  (let [[a b] (augment-location "ab")]
+    (is (= [a]   (join a nil)))
+    (is (= [b]   (join nil b)))
+    (is (= [a b] (join a b)))))
+
+(deftest check-join-on-strings
+  (is (= "ab" (join "a" "b")))
+  (is (= "a"  (join "a" nil)))
+  (is (= "b"  (join nil "b"))))
