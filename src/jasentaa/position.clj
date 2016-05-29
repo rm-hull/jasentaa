3f9fdeb17d35ac224d13b06712709e37c45cf0c4
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-29 20:45:21 +0100

    Add (absolute) offset & some position tests

--- src/jasentaa/position.clj
+++ src/jasentaa/position.clj
@@ -1,21 +1,27 @@
 (ns jasentaa.position)
 
-(defrecord Location [char line col])
+(defrecord Location [char line col offset])
 
 (defn augment-location
   ([text]
-   (augment-location text 1 1))
+   (augment-location text 1 1 0))
 
-  ([text line col]
+  ([text line col offset]
    (let [ch (first text)]
      (if-not (nil? ch)
        (cons
-	 (Location. ch line col)
-         (lazy-seq
-           (augment-location
-             (rest text)
-             (if (= ch \newline) (inc line) line)
-             (if (= ch \newline) 1 (inc col)))))))))
+	 (Location. ch line col offset)
+	 (lazy-seq
+	   (augment-location
+	     (rest text)
+	     (if (= ch \newline) (inc line) line)
+	     (if (= ch \newline) 1 (inc col))
+	     (inc offset))))))))
 
 (defn strip-location [augmented-text]
   (apply str (map :char augmented-text)))
+
+(defn parse-exception [location]
+  (java.text.ParseException.
+    (str "Failed to parse text at line: " (:line location) ", col: " (:col location))
+    (int (:offset location))))
---
+++ test/jasentaa/position_test.clj
@@ -0,0 +1,34 @@
+(ns jasentaa.position-test
+  (:require
+    [clojure.test :refer :all]
+    [jasentaa.position :refer :all])
+  (:import
+    [jasentaa.position Location]))
+
+(deftest check-augment-then-strip
+  (is (= "the quick brown fox"
+	 (strip-location
+	   (augment-location
+	     "the quick brown fox")))))
+
+(deftest check-augment-location-plain-string
+  (is (nil? (augment-location "")))
+  (is (= (augment-location "Hello\nWorld!")
+	 (list
+	   (Location. \H 1 1 0)
+	   (Location. \e 1 2 1)
+	   (Location. \l 1 3 2)
+	   (Location. \l 1 4 3)
+	   (Location. \o 1 5 4)
+	   (Location. \newline 1 6 5)
+	   (Location. \W 2 1 6)
+	   (Location. \o 2 2 7)
+	   (Location. \r 2 3 8)
+	   (Location. \l 2 4 9)
+	   (Location. \d 2 5 10)
+	   (Location. \! 2 6 11)))))
+
+(deftest check-exception
+  (is (thrown-with-msg? java.text.ParseException
+	#"Failed to parse text at line: 6, col: 31"
+	(throw (parse-exception (Location. \Y 6 31 321))))))
