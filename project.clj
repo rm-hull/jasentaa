148be8376ee5123b16b5c58534610bde5156df5b
Richard Hull <rm_hull@yahoo.co.uk>
2016-06-05 21:26:48 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rm-hull/jasentaa "0.2.1"
+(defproject rm-hull/jasentaa "0.2.2"
   :description "A parser-combinator library in Clojure"
   :url "https://github.com/rm-hull/jacentaa"
   :license {
