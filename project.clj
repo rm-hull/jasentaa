a25805bb2028d1b88fb74ec246aa253c9f6a4648
Richard Hull <rm_hull@yahoo.co.uk>
2016-10-07 00:51:57 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject rm-hull/jasentaa "0.2.3"
+(defproject rm-hull/jasentaa "0.2.4-SNAPSHOT"
   :description "A parser-combinator library in Clojure"
   :url "https://github.com/rm-hull/jacentaa"
   :license {
     :name "The MIT License (MIT)"
     :url "http://opensource.org/licenses/MIT"}
-  :dependencies [
-    [org.clojure/clojure "1.8.0"]]
+  :dependencies []
   :scm {:url "git@github.com:rm-hull/jasentaa.git"}
   :source-paths ["src"]
   :jar-exclusions [#"(?:^|/).git"]
@@ -18,5 +17,7 @@
     :dev {
       :global-vars {*warn-on-reflection* true}
       :plugins [
-        [lein-codox "0.9.5"]
-        [lein-cloverage "1.0.6"]]}})
+        [lein-codox "0.10.0"]
+        [lein-cloverage "1.0.7"]]
+      :dependencies [
+        [org.clojure/clojure "1.8.0"]]}})
