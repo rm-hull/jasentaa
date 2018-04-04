0d31c6f9a579fe493ab4db92a818130e2c0efc8b
Frozenlock <frozenlock@gmail.com>
2018-04-04 11:39:07 -0400

    Add testing capabilities for the cljs side of things

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
     :name "The MIT License (MIT)"
     :url "http://opensource.org/licenses/MIT"}
   :scm {:url "https://github.com/rm-hull/jasentaa.git"}
-  :source-paths ["src"]
+  :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :jar-exclusions [#"(?:^|/).git"]
   :codox {
-    :source-paths ["src"]
+    :source-paths ["src/clj" "src/cljc" "src/cljs"]
     :output-path "doc/api"
     :doc-files [
       "doc/worked-example-1.md"
@@ -20,12 +20,22 @@
     :themes [:default [:google-analytics {:tracking-code "UA-39680853-9" }]]}
   :min-lein-version "2.6.1"
   :profiles {
-    :dev {
-      :global-vars {*warn-on-reflection* true}
-      :plugins [
-        [lein-codox "0.10.3"]
-        [lein-cloverage "1.0.10"]
-        [lein-cljfmt "0.5.7"]]
-      :dependencies [
-        [org.clojure/clojure "1.9.0"]
-        [google-analytics-codox-theme "0.1.0"]]}})
+             :dev {
+                   :global-vars {*warn-on-reflection* true}
+                   :plugins [[lein-doo "0.1.10"]
+                             [lein-codox "0.10.3"]
+                             [lein-cloverage "1.0.10"]
+                             [lein-cljfmt "0.5.7"]]
+                   :dependencies [
+                                  [org.clojure/clojure "1.9.0"]
+                                  [org.clojure/clojurescript "1.10.238"]
+                                  [google-analytics-codox-theme "0.1.0"]]}}
+  :aliases {"test" ["do" "clean," "test,"
+                    "doo" "rhino" "test" "once"]}
+  :doo {:paths {:rhino "lein run -m org.mozilla.javascript.tools.shell.Main"}}
+
+  :cljsbuild {:builds
+              [{:id "test"
+                :source-paths ["src" "test"]
+                :compiler {:output-to "target/unit-test.js"
+                           :optimizations :whitespace}}]})
